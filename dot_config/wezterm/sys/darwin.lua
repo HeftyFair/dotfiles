eb67640f5565052a6b417efe955191ac484a61e2
HeftyFair <124026807+HeftyFair@users.noreply.github.com>
2025-05-06 22:55:50 +0800

    Update .config/wezterm/appearance/font.lua Update .config/wezterm/sys/darwin.lua Update .zsh/alias.zsh Update .zsh/darwin.zsh

--- dot_config/wezterm/sys/darwin.lua
+++ dot_config/wezterm/sys/darwin.lua
@@ -1,28 +1,27 @@
-
 local wezterm = require 'wezterm';
-
 
 local M = {}
 local keys = {}
 
 wezterm.on(
-    "open-uri",
-    function(window, pane, uri)
-        local start, match_end = uri:find("file://")
-        if start == 1 then
-            local file = uri:sub(match_end + 1)
-            window:perform_action(
-                wezterm.action {SpawnCommandInNewWindow = {args = {"/opt/homebrew/bin/nvim", file}}},
-                pane
-            )
-            return false
-        end
+  "open-uri",
+  function(window, pane, uri)
+    local start, match_end = uri:find("file://")
+    if start == 1 then
+      local file = uri:sub(match_end + 1)
+      window:perform_action(
+        wezterm.action { SpawnCommandInNewWindow = { args = { "/opt/homebrew/bin/nvim", file } } },
+        pane
+      )
+      return false
     end
+  end
 )
 
 
 table.insert(keys, {
-  key = ",", mods = "SUPER",
+  key = ",",
+  mods = "SUPER",
   action = wezterm.action.SpawnCommandInNewTab({
     cwd = wezterm.home_dir,
     args = { "/opt/homebrew/bin/nvim", wezterm.config_file },
@@ -31,12 +30,13 @@ table.insert(keys, {
 
 M.config = {
   keys = keys,
-	-- window_background_opacity = 0.80,
-	window_decorations = "INTEGRATED_BUTTONS | RESIZE",
-	macos_window_background_blur = 35,
-	front_end = "WebGpu",
-	native_macos_fullscreen_mode = true,
-	webgpu_power_preference = "HighPerformance",
+  window_background_opacity = wezterm.gui and wezterm.gui.get_appearance():find("Dark") and 0.80 or 1.0,
+
+  window_decorations = "INTEGRATED_BUTTONS | RESIZE",
+  macos_window_background_blur = 35,
+  front_end = "WebGpu",
+  native_macos_fullscreen_mode = true,
+  webgpu_power_preference = "HighPerformance",
 }
 
 return M
